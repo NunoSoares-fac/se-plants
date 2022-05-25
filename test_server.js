8b6904bf3b4d2814449e1c17c317f35ef5f05345
Joao <joaorocha95@hotmail.com>
2022-05-25 11:57:24 +0100

    Added server response to update led

--- test_server.js
+++ test_server.js
@@ -2,7 +2,7 @@ var dgram = require('dgram');
 var fs = require("fs");
 
 const server = dgram.createSocket('udp4');
-
+var clientP = 0;
 server.on('error', (err) => {
   console.log(`server error:\n${err.stack}`);
   server.close();
@@ -17,7 +17,18 @@ server.on('message', (msg, rinfo) => {
             console.log("Success! JSON file updated!")
         });
     });
+  var x= Math.round(Math.random())
+  server.send(msg, rinfo.port, 'localhost',function(error){
+  if(error){
+    client.close();
+  }else{
+    console.log(x);
+  }
+
 });
+});
+
+
 
 server.on('listening', () => {
   const address = server.address();
