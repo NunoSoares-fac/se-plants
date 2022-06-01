96ac804790f23e429a36766f986e44f0cfadae00
Joao <joaorocha95@hotmail.com>
2022-06-01 11:30:02 +0100

    abc

--- script.sh
+++ script.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 # run all servers
-#gnome-terminal -- node server.js;
-gnome-terminal -- node test_server.js;
-gnome-terminal -- node index.js;
+
+node test_server.js &
+node server.js &
