26e689c71324cc30d05045cb69346650e5a4bcaa
Eduardo Santos <edu.2000.kill@gmail.com>
2022-06-06 21:56:40 +0100

    remove useless files

--- arduino/plant.cc/plant.cc.ino
+++ arduino/plant.cc/plant.cc.ino
@@ -1,24 +1,52 @@
-
 #include <SPI.h>
 #include <WiFi.h>
 #include <WiFiUdp.h>
+#include <DHT.h>
 
-#define WIFI_SSID "OMEN-HP"   //  your network SSID (name)
-#define WIFI_PASS "uVe2f6Gr"  // your network password
+// wifi use 4,7,10,11,12,13,9 have a led on shild
 
-// UDP
+#define WIFI_SSID "POCO X3 NFC"   //  your network SSID (name)
+#define WIFI_PASS "aaaaaaaa"  // your network password
+
+#define DHTType DHT22
+#define DHTdataPin 2
+
+#define LDRAnalogicIn A0
+#define DigitalLed1 8
+#define DigitalLed2 3
+#define DigitalLed3 5
+
+DHT dht = DHT(DHTdataPin, DHTType);
+
+// UDP https://arduino-esp8266.readthedocs.io/en/latest/esp8266wifi/udp-class.html
 WiFiUDP UDP;
-char packet[255];
-char reply[] = "Packet received!";
+unsigned int localUdpPort = 4210;
 
-IPAddress remoteIP(10,42,0,1);
-#define remotePort 22222
+#define packetBufferSize 35
+char packetBuffer[packetBufferSize];
+
+#define replyBufferSize 75
+char replyBuffer[replyBufferSize];
+
+IPAddress remoteIP(192,168,99,173);
+#define remotePort 41234
 
 void setup() {
   // Setup serial port
   Serial.begin(115200);
   Serial.println();
+
+  pinMode(DigitalLed1, OUTPUT);
+  digitalWrite(DigitalLed1,LOW);
+
+  pinMode(DigitalLed2, OUTPUT);
+  digitalWrite(DigitalLed2,LOW);
+
+  pinMode(DigitalLed3, OUTPUT);
+  digitalWrite(DigitalLed3,LOW);
   
+  dht.begin();
+
   // Begin WiFi
   WiFi.begin(WIFI_SSID, WIFI_PASS);
   
@@ -26,37 +54,74 @@ void setup() {
   Serial.print("Connecting to ");
   Serial.print(WIFI_SSID);
   // Loop continuously while WiFi is not connected
-  while (WiFi.status() != WL_CONNECTED)
-  {
+  while (WiFi.status() != WL_CONNECTED) {
     delay(100);
     Serial.print(".");
   }
   
   // Connected to WiFi
-  Serial.println();
-  Serial.print("Connected! IP address: ");
+  Serial.print("\nConnected! IP address: ");
   Serial.println(WiFi.localIP());
+
+  UDP.begin(localUdpPort);
 }
- 
+
+int lightness;
+char humStr[8];
+char temStr[8];
+
 void loop() {
-  strcpy(packet, "asdasdas\n");
+  unsigned long time = millis();
+  
+  int packetSize = UDP.parsePacket();
+  if(packetSize) {
+    UDP.read(packetBuffer, packetBufferSize);
+
+    packetBuffer[34] = '\0';
+    Serial.println(packetBuffer);
+    
+    char led1 = packetBuffer[9];
+    if(led1 == '0') {
+      digitalWrite(DigitalLed1,LOW);
+    } else if(led1 == '1') {
+      digitalWrite(DigitalLed1,HIGH);
+    }
+
+    char led2 = packetBuffer[20];
+    if(led2 == '0') {
+      digitalWrite(DigitalLed2,LOW);
+    } else if(led2 == '1') {
+      digitalWrite(DigitalLed2,HIGH);
+    }
+
+    char led3 = packetBuffer[31];
+    if(led3 == '0') {
+      digitalWrite(DigitalLed3,LOW);
+    } else if(led1 == '1') {
+      digitalWrite(DigitalLed3,HIGH);
+    }
+    
+  }
+  
+  lightness = analogRead(LDRAnalogicIn);
+  
+  dtostrf(dht.readHumidity(), 5, 2, humStr);
+  dtostrf(dht.readTemperature(), 5, 2, temStr);
+  
+  sprintf(replyBuffer,
+    "\{\"temperature\":\"%s\",\"humidity\":\"%s\",\"luminosity\":\"%d\"\}",  
+    temStr, 
+    humStr, 
+    lightness);
+
   UDP.beginPacket(remoteIP, remotePort);
-  UDP.write(packet, strlen(packet));
+  UDP.write(replyBuffer, strlen(replyBuffer));
   UDP.endPacket();
 
-  delay(1000);
+  Serial.println(replyBuffer);
 
-  /*int packetSize = UDP.parsePacket();
-  if (packetSize) {
-    Serial.print("Received packet! Size: ");
-    Serial.println(packetSize); 
-    int len = UDP.read(packet, 255);
-    if (len > 0)
-    {
-      packet[len] = '\0';
-    }
-    Serial.print("Packet received: ");
-    Serial.println(packet);
-  }*/
- 
+  while(millis() - time <= 500) {
+    delay(50);
+  }
+
 }
